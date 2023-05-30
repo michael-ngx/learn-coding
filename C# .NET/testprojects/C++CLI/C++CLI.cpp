c5ae3b49ed1d2643e56aa9c3d981edb93329ac95
michael.ngx <nguyenvietminh812@gmail.com>
2023-05-30 11:29:50 -0400

    Reorganized c# .net

--- C# .NET/testprojects/C++CLI/C++CLI.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-
-#include "C++CLI.h"
-
-void CCLI::Class1::Test()
-{
-	Console::WriteLine("Hello World!");
-}
--- C# .NET/testprojects/ClassLibrary1/ClassLibrary1.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-
-#include "ClassLibrary1.h"
-
-void ClassLibrary1::MyClass::Test()
-{
-    Console.WriteLine("Hello World!");
-}
